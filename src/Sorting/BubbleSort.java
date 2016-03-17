58326f1e109c6e170d3a87efdb928c7073f88cee
Andrew Jones <andrew_jones@brown.edu>
2016-03-17 16:25:45 -0400

    Added Bubblesort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -3,12 +3,25 @@ public class BubbleSort {
 	/**
 	 * bubbleSort				sorts an array of integers using bubble sort
 	 * 
-	 * @param unsortedArray		an unsorted array of integers
+	 * @param array		an unsorted array of integers
 	 * @return					the input array, sorted least to greatest
 	 */
-	public static int[] bubbleSort(int[] unsortedArray) {
-		//TODO: S1 implements bubble sort
-		
-		return null;
-	}
+  public static int[] bubbleSort(int array[]) {
+    boolean unsorted = true;
+    int holder;
+    
+    while (unsorted) {
+      unsorted = false;
+      for (int i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+          holder = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = holder;
+          unsorted = true;
+        }
+      }
+    }
+
+    return array;
+  }
 }
